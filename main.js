0309e697107fc8e14d6294d0d96e0a3fea931640
sgat6 <122242962+sgat6@users.noreply.github.com>
2023-02-13 22:03:12 +0900

    Create main.js

---
+++ main.js
@@ -0,0 +1,17 @@
+const fs = require('fs')
+const http = require('http')
+
+const server = http.createServer(function (req, res) {
+
+  // FS=FileSystem
+  fs.readFile('./bot.js', function (err, data) {
+    res.end("THehosf!")
+  })
+
+}).listen(8005, function () {
+
+  console.log('server running at http://127.31.128.93:8005')
+
+})
+
+require('./main.js')
